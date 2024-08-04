15cc1d13d2c4ef41bb1cf3c42738b756ea2b262c
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-08-04 23:19:44 +0900

    쿼리 입력 값 받고 조회하기

--- src/location/dtos/get-all-location.dto.ts
+++ src/location/dtos/get-all-location.dto.ts
@@ -1,17 +1,6 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/core-output.dto';
-
-@ObjectType()
-export class Location {
-  @Field(() => String)
-  placeName: string;
-
-  @Field(() => Int)
-  placeNumber: number;
-
-  @Field(() => Int)
-  placeVisitorCount: number;
-}
+import { Location } from './location.dto';
 
 @ObjectType()
 export class GetAllLocationOutput extends CoreOutput {
--- src/location/location.resolver.ts
+++ src/location/location.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { LocationService } from './location.service';
 import { GetAllLocationOutput } from './dtos/get-all-location.dto';
+import { GetLocationInput, GetLocationOutput } from './dtos/get-location.dto';
 
 @Resolver()
 export class LocationResolver {
@@ -9,5 +10,12 @@ export class LocationResolver {
   @Query(() => GetAllLocationOutput)
   async getAllLocation(): Promise<GetAllLocationOutput> {
     return this.locationService.getAllLocation();
+  }
+
+  @Query(() => GetLocationOutput)
+  async getLocation(
+    @Args('input') input: GetLocationInput,
+  ): Promise<GetLocationOutput> {
+    return this.locationService.getLocation(input);
   }
 }
